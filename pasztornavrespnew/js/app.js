b44925c7241680d463bd68fbaef494b14694e81a
thenx6 <adrianokisssu@gmail.com>
2021-06-30 15:56:27 +0200

    PasztorSzerdaNapVege
    Minden valtozas itt van szerdai nap vegeig

--- pasztornavrespnew/js/app.js
+++ pasztornavrespnew/js/app.js
@@ -1,11 +1,14 @@
 $("document").ready(function () {
   $(".navtoggler").on("click", function () {
-    $(".togglenav").removeClass("hide"), $(".navtogglerx").removeClass("hide");
-    $(".navtoggler").addClass("hide");
+    $(".togglenav").slideToggle("slow"),
+      $(".togglenav").removeClass("hide"),
+      $(".navtogglerx").removeClass("hide"),
+      $(".navtoggler").addClass("hide");
   });
 
   $(".navtogglerx").on("click", function () {
-    $(".togglenav").addClass("hide"),
+    $(".togglenav").slideToggle("slow"),
+      $(".togglenav").addClass("hide"),
       $(".navtoggler").removeClass("hide"),
       $(".navtogglerx").addClass("hide");
   });
